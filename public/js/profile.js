d136d9ddd10b926596d5c59afc44643606371611
neilburt <neil.burt@comcast.net>
2021-07-22 22:26:15 -0500

    Built out the last couple handlebars files, completed profile.js.

---
+++ public/js/profile.js
@@ -0,0 +1,40 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const post_body = document.querySelector('#post-body').value.trim();
+
+  if(title && post_body){
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({title, post_body}),
+      headers: {'Content-Type': 'application/json'}
+    });
+
+    if(response.ok){
+      document.location.replace('/profile');
+    }else{
+      alert('failed to post content');
+    }
+  }
+};
+
+const deleteBtnHandler = async (event) => {
+  if(event.target.hasAttribute('data-id')){
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE'
+    });
+
+    if(response.ok){
+      document.location.replace('/profile');
+    }else{
+      alert('failed to delete post');
+    }
+  }
+};
+
+document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
+
+document.querySelector('#post-list').addEventListener('click', deleteBtnHandler);
