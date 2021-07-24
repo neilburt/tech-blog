2538351102b98429e6db536cee530bb0719e2db1
neilburt <neil.burt@comcast.net>
2021-07-24 01:37:05 -0500

    Further fixing of routes and handlebars.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,10 +3,10 @@ const User = require('../../models/User.js');
 
 router.post('/', async (req, res) => {
   try{
-    const userData = await User.create(req.body);
+    const newUser = await User.create(req.body);
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.user_id = newUser.id;
       req.session.logged_in = true;
 
       res.status(200).json(err);
